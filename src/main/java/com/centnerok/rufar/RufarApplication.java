5b8532ecd25b29bb1635cb9f2a5eb30d0b0e476b
Andrew <centner36@gmail.com>
2026-06-26 21:31:07 +0300

    spring init

---
+++ src/main/java/com/centnerok/rufar/RufarApplication.java
@@ -0,0 +1,13 @@
+package com.centnerok.rufar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RufarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RufarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centnerok/rufar/RufarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centnerok.rufar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RufarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
