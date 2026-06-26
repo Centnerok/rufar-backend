eff037429896b5d6d8ac270a0774a614770d0b58
Andrew <centner36@gmail.com>
2026-06-26 23:17:21 +0300

    Fixed UserDto mistake

--- src/main/java/com/centnerok/rufar/model/dto/ProductDto.java
+++ src/main/java/com/centnerok/rufar/model/dto/ProductDto.java
@@ -1,5 +1,7 @@
 package com.centnerok.rufar.model.dto;
 
+import com.centnerok.rufar.model.entity.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ProductDto {
     private Long id;
-    private Long userId;
+    private User userId;
     private String name;
     private String description;
     private String photoPath;
